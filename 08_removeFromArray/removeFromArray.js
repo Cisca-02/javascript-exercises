1b588f6be756f5924bac16b35ca5b3230571fbf8
Cisca <233951544+CiscaSolana@users.noreply.github.com>
2025-12-10 08:53:18 +0100

    Exercise 8 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,12 +1,13 @@
 //funzione che accetta come parametri un array e un elemento da rimuovere eventualmente dall'array
-const removeFromArray = function(arr, toRemove) {
-    let newArr = arr.filter(elem => {
-        if(elem === toRemove){
-            return false;
-        }
-        return true;
-    });
+const removeFromArray = function(arr, ...args) {
+    return  arr.filter(elem => !args.includes(elem));
 };
+
+//dati
+let arrayToModify = [1,2,3,4];
+let toRemove = 3;
+
+removeFromArray(arrayToModify, toRemove);
 
 // Do not edit below this line
 module.exports = removeFromArray;
