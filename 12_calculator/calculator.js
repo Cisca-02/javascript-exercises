21e3ca5b287b5a4c7b40a26a8a69f5455125d51b
Cisca <233951544+CiscaSolana@users.noreply.github.com>
2025-12-13 16:01:11 +0100

    Exercise done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(addends) {
+	return addends.reduce((total, num) => total+num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(factors) {
+  return factors.reduce((total, num) => total*num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {  
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for(let i=num; i>=1; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
